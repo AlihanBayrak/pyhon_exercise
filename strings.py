0a1c1fe1d3c0290eb9b1636665c6047bd2644872
AlihanBayrak <alob43bayram@gmail.com>
2019-12-09 14:24:10 +0300

    String Functions

--- strings.py
+++ strings.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 mesaj = "Selamunaleykum Dünya" #* 20 krakteden oluşur
 print(mesaj[2:5]) #ikiden beşe kadar
-yeniMesaj = mesaj[:2]
+yeniMesaj = mesaj[:2] #Baştan ikiye kadar
 print(yeniMesaj)
+# aslında bu yaptığımız işlemi substring() işlemidir.
 
+print(len(mesaj)) #bize değişkenin karakter sayısını verir.
+
+print(mesaj[19:20])
+yeniMesaj_2 = mesaj[len(mesaj)-1:len(mesaj)]#yukardaki pritle aynı çıktıyı verir
+print(yeniMesaj_2)
 
 
 
