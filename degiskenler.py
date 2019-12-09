c3b9ccd6bd9953b22fba03a90668e2104409d1ea
AlihanBayrak <alob43bayram@gmail.com>
2019-12-09 14:03:48 +0300

    veriables defined

--- degiskenler.py
+++ degiskenler.py
@@ -1,2 +1,12 @@
-# -*- coding: utf-8 -*-
+#değişken veri tiperinde belirli degerleri tuta
+x = 20 #integer tanımlama
+tc = "20202020208"
+y = "20" #string tanımlama
+isim = "Alihan" #string tanımlama
+soyisim = "Bayrak" #string tanımlama
+floatsayi = 10
 
+print(float(tc) + 10) #float() bunla tür dönüşümü yaptık bir string ifade olan tc yi float a çevirdik
+print(type(floatsayi)) #tpye() fonlsiyonu değişkenin türünü verir
+print(type(tc))
+print(type(isim))
