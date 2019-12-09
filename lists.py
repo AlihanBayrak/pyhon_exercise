dfb12264d4f38d7da4cc130b336e1125d0b77af4
AlihanBayrak <alob43bayram@gmail.com>
2019-12-09 16:00:35 +0300

    lists constructor

--- lists.py
+++ lists.py
@@ -18,6 +18,25 @@ ogrenciler.remove("Yasin")
 print(ogrenciler)
 
 #list constructor(oluşturucu)
-sehirler = list(("Erzurum", "İstanbul"))
+sehirler = list(("Erzurum", "İstanbul", "Erzurum"))
 print(sehirler)
+print(len(sehirler))
+
+#diğer fonksiyonlar
+#print(sehirler.clear()) #temizlendi :)
+print("Erzurum sayısı = " + str(sehirler.count("Erzurum")))
+print("Erzurum indexi = " + str(sehirler.index("Erzurum")))
+sehirler.pop(1) # çıktı
+sehirler.insert(0, "İstanbul") #ekleme
+sehirler.reverse() # takla attırma :) ['Erzurum', 'Erzurum', 'İstanbul']
+print(sehirler)
+
+sehirler3 = sehirler.copy()
+
+sehirler2 = sehirler
+sehirler2[0] = "İzmir"
+print(sehirler2)
+print(sehirler)
+print(sehirler3)
+
 
