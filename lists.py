d026b7dc8d1c3f9d976dd4acb7fddf27ff75bb4b
AlihanBayrak <alob43bayram@gmail.com>
2019-12-09 15:42:25 +0300

    lists constructor

---
+++ lists.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+#ogrenci1 = "Alihan"
+#ogrenci2 = "Aydın"
+#ogrenci3 = "Arzu"
+#ogrenci4 = "Yasin"
+#
+#print(ogrenci1)
+#print(ogrenci2)
+#print(ogrenci3)
+#print(ogrenci4)
+
+ogrenciler = ["Alihan", "Aydın", "Arzu", "Yasin"]
+
+ogrenciler.append("Talha")
+print(ogrenciler[3]) #Yasin
+ogrenciler.remove("Yasin")
+print(ogrenciler)
+
+#list constructor(oluşturucu)
+sehirler = list(("Erzurum", "İstanbul"))
+print(sehirler)
+
