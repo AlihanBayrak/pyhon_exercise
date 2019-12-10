20153d5738d0e8b64fd0b3b54ec57f6dd94c5772
AlihanBayrak <alob43bayram@gmail.com>
2019-12-10 09:34:46 +0300

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+lights = ["red","yellow","green"]
+
+currentLights = lights[2]
+
+print(currentLights)
+
+if currentLights == "red":
+    print("STOP!")
+
+if currentLights == "yellow":
+    print("READY!")    
+    
+if currentLights == "green":
+    print("GO!")    
