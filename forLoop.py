71e1ac2df16daa1717366252077d24010c5a1afd
AlihanBayrak <alob43bayram@gmail.com>
2019-12-10 10:08:22 +0300

    forLoop

---
+++ forLoop.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+sehirler =["Erzurum","İstanbul","Ankara"]
+
+for sehir in sehirler:
+    print(sehir + "için kod =" + sehir[0:3])
+
+#output
+#Erzurumiçin kod =Erz
+#İstanbuliçin kod =İst
+#Ankaraiçin kod =Ank
