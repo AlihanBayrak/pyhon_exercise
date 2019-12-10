62e73a1d9ec71b0c064ac236d79b862edfdc8597
AlihanBayrak <alob43bayram@gmail.com>
2019-12-10 11:16:47 +0300

    homework

---
+++ workshops/kelimeSiralamaca.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+cumle = "buğün hava çok soğuk çünkü burası erzurum"
+
+kelimeler = cumle.split()
+
+kelimeler.sort()
+
+print(kelimeler)
+
+for kelime in kelimeler:
+    print(kelime)
